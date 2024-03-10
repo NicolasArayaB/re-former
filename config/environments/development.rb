4d9767c0daf99f61e2ca4f484c067cd334b64e62
NicolasArayaB <nicolasaraya@gmail.com>
2024-03-10 14:33:44 +0000

    Finish form project

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.hosts << "3000-nicolasarayab-reformer-9jqbrgqwbpz.ws-us108.gitpod.io"
 end
